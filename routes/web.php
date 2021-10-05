99c91af5ab95a3f5dc73ac4745481846dc2c0db9
jessinthecloud <git@github.com>
2021-10-04 22:42:56 -0400

    more crude data test grab + links

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,63 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/scrape', function () {
+
+    $base_url = "https://nwdb.info/db/";
+    
+    /* 
+    /_app/pages/db/[entryType]/[itemId].svelte-b28fafcb.js
+    
+    <script type="module">
+    import { start } from "/_app/start-a5b768b0.js";
+    start({
+    target: document.querySelector("#svelte"),
+    paths: {"base":"","assets":""},
+    session: {},
+    host: "nwdb.info",
+    route: true,
+    spa: false,
+    trailing_slash: "never",
+    hydrate: {
+     status: 200,
+    error: null,
+    nodes: [
+        import("/_app/pages/__layout.svelte-77a3b737.js"),
+        import("/_app/pages/db/[entryType]/[itemId].svelte-b28fafcb.js")
+    ],
+    page: {
+        host: "nwdb.info",
+        path: "/db/item/2hfishingpole_ancient",
+        query: new URLSearchParams(""),
+        params: {"entryType":"item","itemId":"2hfishingpole_ancient"}
+     }
+    }
+    });
+    </script> 
+    */
+     
+    $url = $base_url.'shops/syndicate-armory/page/1';
+    $filename = 'shops-syndicate-armory-page-1.html';
+
+    dump('URL: ' . $url);
+//        continue;
+
+    $response = Http::accept( 'application/json' )->get( $url );
+
+    dump( $response, $response->body() );
+
+
+    if ( $response->successful() ) {
+        Storage::disk( 'local' )->put(
+            storage_path( 'app/json' ) . '/' . $filename,
+            $response->body()
+        );
+        dump(storage_path( 'app/json' ) . '/' .$filename);
+    } else {
+        dump( 'Request failed: ', $response->status(), $response->headers(), $response->body() );
+    }
+});
+
 Route::get('/scrape/category/{category}/{type?}', function (string $category, string $type=null) {
     
     $base_url = "https://newworldfans.com/db/category/";
