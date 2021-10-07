58312385315a2e19df3252a5e86286067c8dc6fc
jessinthecloud <code@jessinthe.cloud>
2021-10-06 23:34:46 -0400

    clarify/add props

--- app/Models/Recipes/Recipe.php
+++ app/Models/Recipes/Recipe.php
@@ -2,16 +2,19 @@
 
 namespace App\Models\Recipes;
 
+use App\Models\Items\Item;
 use Illuminate\Support\Collection;
 
 abstract class Recipe
 {
     /** array of Ingredient models */
     protected Collection $ingredients;
-    protected Tradeskill $required_skill;
-    protected Station $required_station; // including tier/level
-    // skill level needed to craft this recipe
-    protected int $required_skill_level;
+    // skill and level needed to craft this recipe
+    protected Tradeskill $required_skill; // including level
+    protected CraftingStation $required_station; // including tier/level
     // xp gained from crafting the recipe
     protected int $experience_points;
+    protected int $territory_standing;
+    protected Item $result;
+    
 }
