73c4a4a342874d631c28190d86f0fd4c419a1ad3
Jess <code@jessinthe.cloud>
2021-10-07 00:31:45 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,37 +23,6 @@ Route::get('/get-nwf-data', [App\Http\Controllers\NwfController::class, 'getAll'
 Route::get('/get-nwf-data/{category}', [App\Http\Controllers\NwfController::class, 'category']);
 Route::get('/get-nwf-data/{category}/{type}', [App\Http\Controllers\NwfController::class, 'categoryType']);
 
-/* 
-    /_app/pages/db/[entryType]/[itemId].svelte-b28fafcb.js
-    
-    <script type="module">
-    import { start } from "/_app/start-a5b768b0.js";
-    start({
-    target: document.querySelector("#svelte"),
-    paths: {"base":"","assets":""},
-    session: {},
-    host: "nwdb.info",
-    route: true,
-    spa: false,
-    trailing_slash: "never",
-    hydrate: {
-     status: 200,
-    error: null,
-    nodes: [
-        import("/_app/pages/__layout.svelte-77a3b737.js"),
-        import("/_app/pages/db/[entryType]/[itemId].svelte-b28fafcb.js")
-    ],
-    page: {
-        host: "nwdb.info",
-        path: "/db/item/2hfishingpole_ancient",
-        query: new URLSearchParams(""),
-        params: {"entryType":"item","itemId":"2hfishingpole_ancient"}
-     }
-    }
-    });
-    </script> 
-    */
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
